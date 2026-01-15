1fc4665ac5e470d6ae33c0f0626d1017075b14fc
sadiqull-ameen <sadiqull840@gmail.com>
2026-01-15 07:07:14 +0530

    Add @Id annotation to Activity class id field
    Added @Id annotation to the 'id' field in Activity class.

--- activityservice/activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/model/Activity.java
@@ -1,3 +1,40 @@
+// package com.fitness.activityservice.model;
+
+// import lombok.AllArgsConstructor;
+// import lombok.Builder;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+// import org.springframework.data.annotation.CreatedDate;
+// import org.springframework.data.annotation.LastModifiedDate;
+// import org.springframework.data.mongodb.core.mapping.Document;
+// import org.springframework.data.mongodb.core.mapping.Field;
+
+// import java.time.LocalDateTime;
+// import java.util.Map;
+
+// @Document(collection = "activities")
+// @Data
+// @Builder
+// @AllArgsConstructor
+// @NoArgsConstructor
+// public class Activity {
+//     private String id;
+//     private String userId;
+//     private ActivityType type;
+//     private Integer duration;
+//     private Integer caloriesBurned;
+//     private LocalDateTime startTime;
+
+//     @Field("metrics")
+//     private Map<String, Object> additionalMetrics;
+
+//     @CreatedDate
+//     private LocalDateTime createdAt;
+
+//     @LastModifiedDate
+//     private LocalDateTime updatedAt;
+// }
+
 package com.fitness.activityservice.model;
 
 import lombok.AllArgsConstructor;
@@ -6,6 +43,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -18,7 +56,10 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Activity {
-    private String id;
+
+    @Id
+    private String id;   // ✅ REQUIRED
+
     private String userId;
     private ActivityType type;
     private Integer duration;
