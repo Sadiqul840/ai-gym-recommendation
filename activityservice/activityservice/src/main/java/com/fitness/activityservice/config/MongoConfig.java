d618359f716186f31833202b4c8f1c0ae0027910
sadiqull-ameen <sadiqull840@gmail.com>
2026-01-15 07:04:08 +0530

    Enable MongoDB auditing in MongoConfig

--- activityservice/activityservice/src/main/java/com/fitness/activityservice/config/MongoConfig.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/config/MongoConfig.java
@@ -1,10 +1,21 @@
+// package com.fitness.activityservice.config;
+
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+// import org.springframework.context.annotation.Configuration;
+
+// @Configuration
+// @EnableAutoConfiguration
+// public class MongoConfig {
+
+// }
+
 package com.fitness.activityservice.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
-@EnableAutoConfiguration
+@EnableMongoAuditing
 public class MongoConfig {
-
 }
+
