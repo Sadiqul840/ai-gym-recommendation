5f652e95a220d767c92a9f4e3dd8fa5ca687966c
sadiqull-ameen <sadiqull840@gmail.com>
2026-01-15 07:03:22 +0530

    Update userServiceWebClient to use new base URL

--- activityservice/activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/config/WebClientConfig.java
@@ -1,6 +1,29 @@
+// package com.fitness.activityservice.config;
+
+// import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.reactive.function.client.WebClient;
+
+// @Configuration
+// public class WebClientConfig {
+
+//     @Bean
+//     @LoadBalanced
+//     public WebClient.Builder webClientBuilder() {
+//         return WebClient.builder();
+//     }
+
+//     @Bean
+//     public WebClient userServiceWebClient(WebClient.Builder webClientBuilder) {
+//         return webClientBuilder
+//                 .baseUrl("http://USER-SERVICE")
+//                 .build();
+//     }
+// }
+
 package com.fitness.activityservice.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -9,15 +32,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
-    @LoadBalanced
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
-    }
-
-    @Bean
-    public WebClient userServiceWebClient(WebClient.Builder webClientBuilder) {
-        return webClientBuilder
-                .baseUrl("http://USER-SERVICE")
+    public WebClient userServiceWebClient() {
+        return WebClient.builder()
+                .baseUrl("https://ai-gym-userservice.onrender.com")
                 .build();
     }
 }
+
