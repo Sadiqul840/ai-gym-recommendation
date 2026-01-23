ae729bb7b438dca75537e10d18bbdce52104ec1a
sadiqull-ameen <sadiqull840@gmail.com>
2026-01-24 03:05:52 +0530

    Disable MongoDB auditing in MongoConfig
    Comment out the @EnableMongoAuditing annotation in MongoConfig.

--- activityservice/activityservice/src/main/java/com/fitness/activityservice/config/MongoConfig.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/config/MongoConfig.java
@@ -12,10 +12,10 @@
 package com.fitness.activityservice.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
+// import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
-@EnableMongoAuditing
+// @EnableMongoAuditing
 public class MongoConfig {
 }
 
