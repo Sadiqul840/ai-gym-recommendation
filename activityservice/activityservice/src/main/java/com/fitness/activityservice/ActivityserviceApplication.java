e7402169b299206a5888dbb2e5e5364cedbba116
sadiqull-ameen <sadiqull840@gmail.com>
2026-01-24 02:57:29 +0530

    Add MongoDB auditing support to application

--- activityservice/activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
@@ -2,13 +2,13 @@ package com.fitness.activityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 @EnableMongoAuditing
 public class ActivityserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActivityserviceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityserviceApplication.class, args);
+    }
 }
