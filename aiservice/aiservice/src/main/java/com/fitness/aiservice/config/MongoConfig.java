5f60b85834d4b04204975e717e6c7c982144262f
sadiqull-ameen <sadiqull840@gmail.com>
2026-01-26 02:30:25 +0530

    Update MongoConfig.java

--- aiservice/aiservice/src/main/java/com/fitness/aiservice/config/MongoConfig.java
+++ aiservice/aiservice/src/main/java/com/fitness/aiservice/config/MongoConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
-@EnableMongoAuditing
+// @EnableMongoAuditing
 public class MongoConfig {
 }
