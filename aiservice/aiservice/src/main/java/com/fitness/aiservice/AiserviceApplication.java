5cfb3133c19602ef80e86a23d907f1b4185567f2
sadiqull-ameen <sadiqull840@gmail.com>
2026-01-29 16:50:07 +0530

    Refactor AiserviceApplication for package scanning

--- aiservice/aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
+++ aiservice/aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
@@ -1,15 +1,29 @@
+// package com.fitness.aiservice;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+// @SpringBootApplication
+// @EnableMongoAuditing
+// public class AiserviceApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(AiserviceApplication.class, args);
+// 	}
+
+// }
+
 package com.fitness.aiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@SpringBootApplication
-@EnableMongoAuditing
+@SpringBootApplication(scanBasePackages = "com.fitness.aiservice")
+@@EnableMongoAuditing
 public class AiserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AiserviceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(AiserviceApplication.class, args);
+    }
 }
