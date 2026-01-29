7079ea005741224ec0a77eccb0aceec4e412b896
sadiqull-ameen <sadiqull840@gmail.com>
2026-01-29 17:09:32 +0530

    Update AiserviceApplication.java

--- aiservice/aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
+++ aiservice/aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
@@ -18,12 +18,14 @@ package com.fitness.aiservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication(scanBasePackages = "com.fitness.aiservice")
-@@EnableMongoAuditing
+@EnableMongoAuditing
 public class AiserviceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AiserviceApplication.class, args);
     }
 }
+
